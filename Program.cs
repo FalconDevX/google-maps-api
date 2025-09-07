cd16660062356c06f8f1fe23c79ac936a0ff2577
SpaceXEngineer <148818352+FalconDevX@users.noreply.github.com>
2025-09-07 22:31:31 +0200

    Enhance Swagger integration in ASP.NET Core app
    Streamlined Swagger and Swagger UI configuration for all environments. Removed development-specific setup and explicitly defined the Swagger endpoint and route prefix.

--- Program.cs
+++ Program.cs
@@ -2,11 +2,8 @@
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddHttpClient<GoogleMapsService>();
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -14,17 +11,15 @@ var app = builder.Build();
 
 app.MapGet("/", () => "API works");
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+app.UseSwagger();
+app.UseSwaggerUI(c =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+    c.RoutePrefix = string.Empty;
+});
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
 
 app.MapControllers();
-
 app.Run();
