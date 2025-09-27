154d6a9be8ae1b36739641ba7cc026bd9f42f720
SpaceXEngineer <148818352+FalconDevX@users.noreply.github.com>
2025-09-27 14:18:23 +0200

    Refactor data download process and update places list
    - Updated `download_data.py` to read places from `places.json` instead of hardcoding a single place.
    - Implemented a loop to fetch nearby places based on both "POPULARITY" and "DISTANCE" rankings.
    - Enhanced error handling for API responses.
    - Updated `output.json` with new data, including changes to user ratings and types for various places.
    - Modified `places.json` to include a new list of places for querying.

--- download_data.py
+++ download_data.py
@@ -1,54 +1,42 @@
 import requests
 import json
 
-nearby_places_url= "http://34.56.66.163/api/GoogleMaps/getNearbyPlacesByName"
-
+nearby_places_url = "http://34.56.66.163/api/GoogleMaps/getNearbyPlacesByName"
 text_places_url = "http://34.56.66.163/api/GoogleMaps/getTextPlacesByName"
 
-access_token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE3NTg5NzIyODksImlzcyI6Ik15QXBwIiwiYXVkIjoiQXBwVXNlcnMiLCJzdWIiOjExLCJuYW1lIjoidGVzdDUifQ.FYobDc89KJZNpplbiJXAF4r_UusN6hZ3xf7tj4LWzAQ"
+access_token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE3NTg5NzYxNjgsImlzcyI6Ik15QXBwIiwiYXVkIjoiQXBwVXNlcnMiLCJzdWIiOjExLCJuYW1lIjoidGVzdDUifQ.G3dkLstTB-ssMTLT3kGyW_jxWo1vyH3tKxLhqkjJsqI"
 
 headers = {
     "Authorization": f"Bearer {access_token}"
 }
 
 data_results = []
-
 unique_place_ids = {}
 
-place = "Kraków Kościuł Mariacki"
+with open("places.json", "r", encoding="utf-8") as f:
+    places = json.load(f)
+
 radius = 500
 
-response = requests.get(
-    nearby_places_url,
-    params={"query": place, "radius": radius, "rankPreference": "POPULARITY"},
-    headers=headers,
-    verify=False
-)
-if response.status_code == 200:
-    data_results.extend(response.json())
-else:
-    print(f"Error: {response.status_code}")
+for place in places:
+    for rank in ["POPULARITY", "DISTANCE"]:
+        response = requests.get(
+            nearby_places_url,
+            params={"query": place, "radius": radius, "rankPreference": rank},
+            headers=headers,
+            verify=False
+        )
 
-for result in data_results:
-    key=result.get("id")
-    if key not in unique_place_ids:
-        unique_place_ids[key] = result
-    
-response = requests.get(
-    nearby_places_url,
-    params={"query": place, "radius": radius, "rankPreference": "DISTANCE"},
-    headers=headers,
-    verify=False
-)
-if response.status_code == 200:
-    data_results.extend(response.json())
-else:
-    print(f"Error: {response.status_code}")
-
-for result in data_results:
-    key=result.get("id")
-    if key not in unique_place_ids:
-        unique_place_ids[key] = result
+        if response.status_code == 200:
+            results = response.json()
+            for result in results:
+                key = result.get("id")
+                if key and key not in unique_place_ids:
+                    unique_place_ids[key] = result
+        else:
+            print(f"Błąd ({response.status_code}) dla {place} [{rank}]")
 
 with open("output.json", "w", encoding="utf-8") as file:
     json.dump(list(unique_place_ids.values()), file, indent=4, ensure_ascii=False)
+
+print(f"✅ Zapisano {len(unique_place_ids)} miejsc do output.json")
